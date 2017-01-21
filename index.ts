116ffcbf0b0dfc2ddbd77bd192fab6f367f9dcee
Talip Tayfur <taliptayfur@gmail.com>
2017-01-21 14:39:19 +0300

    initial commit

---
+++ index.ts
@@ -0,0 +1,30 @@
+import {
+  TreeModel,
+  TreeSettings,
+  RenamableNode,
+  NodeEvent,
+  NodeCreatedEvent,
+  NodeRemovedEvent,
+  NodeRenamedEvent,
+  NodeMovedEvent,
+  NodeSelectedEvent,
+  NodeDestructiveEvent
+} from './src/tree.types';
+
+import { TreeComponent } from './src/tree.component';
+import { TreeModule } from './src/tree.module';
+
+export {
+  TreeModel,
+  TreeSettings,
+  RenamableNode,
+  NodeEvent,
+  NodeCreatedEvent,
+  NodeRemovedEvent,
+  NodeRenamedEvent,
+  NodeMovedEvent,
+  NodeSelectedEvent,
+  NodeDestructiveEvent,
+  TreeComponent,
+  TreeModule
+};
---
+++ src/tree.types.ts
@@ -0,0 +1,60 @@
+export class FoldingType {
+  public static Expanded: FoldingType = new FoldingType('node-expanded');
+  public static Collapsed: FoldingType = new FoldingType('node-collapsed');
+  public static Leaf: FoldingType = new FoldingType('node-leaf');
+
+  public constructor(private _cssClass: string) {
+  }
+
+  public get cssClass(): string {
+    return this._cssClass;
+  }
+}
+
+export interface TreeModel {
+  value: string | RenamableNode;
+  children?: Array<TreeModel>;
+  _status?: TreeStatus;
+  _foldingType?: FoldingType;
+  _indexInParent?: number;
+}
+
+export interface TreeSettings {
+  contextMenu: boolean;
+}
+
+export enum TreeStatus {
+  New,
+  Modified,
+  EditInProgress
+}
+
+export interface RenamableNode {
+  setName(name: string): void;
+  toString(): string;
+}
+
+export interface NodeEvent {
+  node: TreeModel;
+}
+
+export interface NodeSelectedEvent extends NodeEvent {
+}
+
+export interface NodeDestructiveEvent extends NodeEvent {
+  parent: TreeModel;
+}
+
+export interface NodeMovedEvent extends NodeDestructiveEvent {
+}
+
+export interface NodeRemovedEvent extends NodeDestructiveEvent {
+}
+
+export interface NodeCreatedEvent extends NodeDestructiveEvent {
+}
+
+export interface NodeRenamedEvent extends NodeDestructiveEvent {
+  newValue: string | RenamableNode;
+  oldValue: string | RenamableNode;
+}
